dc2bcd970b2ae8f620c511a929f484de0d2b2c27
被时间偷走的回忆 <1085334205@qq.com>
2020-08-03 14:33:58 +0800

    搭建登录页面结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,38 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+
+const Login = () => import(/* webpackChunkName: "Login_Home_Welcome" */ '../components/Login.vue')
+const Home = () => import(/* webpackChunkName: "Login_Home_Welcome" */ '../components/Home.vue')
+
+
 
 Vue.use(VueRouter)
 
+const routerPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return routerPush.call(this, location).catch(error => error)
+}
+
+
   const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  // 错误路由
+    {
+      path: "/404",
+      name: "404",
+      component: () => import(/* webpackChunkName: "Login_Home_Welcome" */ '../components/error/404.vue'),
+      visible: false,
+    },
+    {
+      path: "*",
+      redirect: "/404",
+      visible: false,
+    },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes
 })
 
