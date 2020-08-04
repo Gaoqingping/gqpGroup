c9b4290d25137c98afedfa38f14f77dcf4a37960
被时间偷走的回忆 <1085334205@qq.com>
2020-08-04 15:58:20 +0800

    完成数据统计组件

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -2,24 +2,24 @@
   <div>
     <!-- 面包屑导航区 -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
-    <el-breadcrumb-item :to="{ path: `/home` }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item :to="{ path: `/home` }">订单管理</el-breadcrumb-item>
-    <el-breadcrumb-item :to="{ path: `/home` }">订单列表</el-breadcrumb-item>
+    <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+    <el-breadcrumb-item :to="{ path: '/home' }">订单管理</el-breadcrumb-item>
+    <el-breadcrumb-item :to="{ path: '/home' }">订单列表</el-breadcrumb-item>
     </el-breadcrumb>
 
     <!-- 卡片视图区域 -->
     <el-card>
         <el-row>
-            <el-col :span="8">
-                <el-input placeholder="请输入内容">
-    <el-button slot="append" icon="el-icon-search"></el-button>
-  </el-input>
-            </el-col>
+          <el-col :span="6">
+            <el-input placeholder="请输入内容">
+              <el-button slot="append" icon="el-icon-search"></el-button>
+            </el-input>
+          </el-col>
         </el-row>
     </el-card>
 
     <!-- 订单列表数据 -->
-    <el-table : data="orderlist" border stripe>
+    <el-table :data="orderlist" border stripe>
         <el-table-column type="index"></el-table-column>
         <el-table-column label="订单编号"></el-table-column>
     </el-table>
