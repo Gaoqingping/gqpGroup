7d1c6b3a025cb724cc186fb5cdd226db0d45d9b7
guangtouqiang <386145571@qq.com>
2020-08-06 11:32:38 +0800

    订单列表V5

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -36,7 +36,7 @@
             </template>
         </el-table-column>
         <el-table-column label="操作">
-            <template slot-scope='scope'>
+            <template slot-scope="scope">
                 <el-button size="mini" type="primary" icon="el-icon-edit" @click="showBox"></el-button>
                 <el-button size="mini" type="success" icon="el-icon-location" @click='showProgress'></el-button>
             </template>
@@ -144,7 +144,7 @@ export default {
             this.getOrderList()
         },
         handleCurrentChange(newPage){
-            this,queryInfo,pagenum = newPage
+            this.queryInfo.pagenum = newPage
             this.getOrderList()
         },
         //展示修改地址的对话框
